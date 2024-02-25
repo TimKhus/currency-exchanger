7d6882e8a0e6fda0813128361f49192cf3430ef0
Timur Khusnutdinov <t.m.khusnutdinov@gmail.com>
2024-02-25 20:54:24 +0200

    Currency model

---
+++ src/main/java/com/example/currencyexchanger/models/Currency.java
@@ -0,0 +1,32 @@
+package com.example.currencyexchanger.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="currency")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Currency {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    @NotBlank
+    @Size(max=3)
+    @Column(name = "code", unique = true)
+    String code;
+
+    @NotBlank
+    @Column(name = "full_name")
+    String fullName;
+
+    @Column(name = "sign")
+    String sign;
+}
