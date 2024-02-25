27df595a1335207ad47b7bf09a738f9095c8bad1
Timur Khusnutdinov <t.m.khusnutdinov@gmail.com>
2024-02-25 21:00:19 +0200

    ExchangeRate model

---
+++ src/main/java/com/example/currencyexchanger/models/ExchangeRate.java
@@ -0,0 +1,33 @@
+package com.example.currencyexchanger.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "exchange_rates")
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExchangeRate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    @NotBlank
+    @Column(name = "source_currency")
+    Long sourceCurrencyId;
+
+    @NotBlank
+    @Column(name = "target_currency")
+    Long targetCurrencyId;
+
+    @NotBlank
+    @Column(name = "rate")
+    BigDecimal rate;
+}
