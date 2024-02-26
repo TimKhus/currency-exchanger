edd2808f1e777ced90d8fbb5902b4c160999f211
Timur Khusnutdinov <t.m.khusnutdinov@gmail.com>
2024-02-26 20:52:50 +0200

    Adding CurrencyService

--- src/main/java/com/example/currencyexchanger/repositories/CurrencyRepository.java
+++ src/main/java/com/example/currencyexchanger/repositories/CurrencyRepository.java
@@ -3,5 +3,8 @@ package com.example.currencyexchanger.repositories;
 import com.example.currencyexchanger.models.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
+    Optional<Currency> findByCode(String code);
 }
