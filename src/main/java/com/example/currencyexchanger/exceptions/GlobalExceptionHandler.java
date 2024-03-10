1a5e7377896d1c9bb02283c03107521fe1da5dac
Timur Khusnutdinov <t.m.khusnutdinov@gmail.com>
2024-03-10 18:38:02 +0200

    adding methods to exceptionhandler

--- src/main/java/com/example/currencyexchanger/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/currencyexchanger/exceptions/GlobalExceptionHandler.java
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,4 +23,20 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleEmptyResultDataAccessException(EmptyResultDataAccessException e) {
         return new ResponseEntity<>("No content available", HttpStatus.NO_CONTENT);
     }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        String errorMessage = String.format("Invalid argument type. Expected %s, but received %s", e.getRequiredType(),
+                e.getValue().getClass());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
+        String errorMessage = String.format("Element not found: " + e.getMessage());
+        return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+    }
+
+
+
 }
